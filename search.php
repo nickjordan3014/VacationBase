7840ec7322865ab69cd221adc3f3b944363e95f2
Nicholas Jordan <65570462+nickjordan3014@users.noreply.github.com>
2023-01-25 14:57:23 -0500

    Content Update 1.14
    Search Page Set Up

---
+++ search.php
@@ -0,0 +1,7 @@
+<?php
+    include("search/searchHead.html");
+    include("navigation/nav.html");
+    include("search/searchBody.html");
+    include("header/header.html");
+    include("footer/footer.html");
+?>
