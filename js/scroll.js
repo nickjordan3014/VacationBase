b895ddeec99c05f197182cb49776249e38126976
Nicholas Jordan <nickjordan3014@gmail.com>
2023-01-29 18:39:54 -0500

    Content Update 2.0
    - Added Arrows to image folder
    
    - Updated scroll functionality to go both left and right
    
    - Finished mobile nav bar

--- js/scroll.js
+++ js/scroll.js
@@ -15,7 +15,7 @@ const data = {
     ]
 }
 
-function scroll(id) { // function to move carousel over
+function scrollRight(id) { // function to move carousel over to the right
         let carousel = document.getElementById(id);
         let carouselWidth = carousel.offsetWidth - (carousel.offsetWidth * .2);
         let carouselPosition = carousel.scrollLeft;
@@ -26,18 +26,44 @@ function scroll(id) { // function to move carousel over
         });
 }
 
+function scrollLeft(id) { // function to move carousel over to the right
+    let carousel = document.getElementById(id);
+    let carouselWidth = carousel.offsetWidth - (carousel.offsetWidth * .2);
+    let carouselPosition = carousel.scrollLeft;
+
+    carousel.scrollTo({ //moves the carousel left based on current position and screen size
+        left: carouselPosition = carouselPosition - carouselWidth,
+        behavior: 'smooth',
+    });
+}
+
 function init() { //intializes the function
-    document.getElementById("auto0").onclick = function() { //First Carousel moves if clicked
-        scroll(data.ids[0].scrollid);
+    document.getElementById("autoRight0").onclick = function() { //First Carousel moves right if clicked
+        scrollRight(data.ids[0].scrollid);
     }
-    document.getElementById("auto1").onclick = function() { //First Carousel moves if clicked
-        scroll(data.ids[1].scrollid);
+    document.getElementById("autoLeft0").onclick = function() { //First Carousel moves right if clicked
+        scrollLeft(data.ids[0].scrollid);
     }
-    document.getElementById("auto2").onclick = function() { //First Carousel moves if clicked
-        scroll(data.ids[2].scrollid);
+
+    document.getElementById("autoRight1").onclick = function() { //Second Carousel moves right if clicked
+        scrollRight(data.ids[1].scrollid);
     }
-    document.getElementById("auto3").onclick = function() { //First Carousel moves if clicked
-        scroll(data.ids[3].scrollid);
+    document.getElementById("autoLeft1").onclick = function() { //Second Carousel moves right if clicked
+        scrollLeft(data.ids[1].scrollid);
+    }
+
+    document.getElementById("autoRight2").onclick = function() { //Third Carousel moves right if clicked
+        scrollRight(data.ids[2].scrollid);
+    }
+    document.getElementById("autoLeft2").onclick = function() { //Second Carousel moves right if clicked
+        scrollLeft(data.ids[2].scrollid);
+    }
+
+    document.getElementById("autoRight3").onclick = function() { //Forth Carousel moves right if clicked
+        scrollRight(data.ids[3].scrollid);
+    }
+    document.getElementById("autoLeft3").onclick = function() { //Second Carousel moves right if clicked
+        scrollLeft(data.ids[3].scrollid);
     }
 }
 
