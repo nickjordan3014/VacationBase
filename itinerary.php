3aacf7ad4b885c77f4b926ae9d600442817686c6
MykeriaC <mykeriacooks6@knights.ucf.edu>
2023-01-29 16:23:28 -0500

    itinerary folder
    created the itinerary subfolder inside the pages folder with body and header html file inside, created the itinerary.php page
    
    debugging some SQL to run through it prior to adding the file
    
    **included <link rel="stylesheet" href="style/responsive.css"> in the ideaBase and search header html file!!

---
+++ itinerary.php
@@ -0,0 +1,7 @@
+<?php
+    include("pages/itinerary/itineraryHead.html");
+    include("template/nav.html");
+    include("pages/itinerary/itineraryBody.html");
+    include("template/header.html");
+    include("template/footer.html");
+?>
