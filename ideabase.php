521f7d1bb27187f9c80684f72211fc7fa42ec8ff
Nicholas Jordan <nickjordan3014@gmail.com>
2023-01-30 01:22:03 -0500

    Content Update 1.21
    Took away header file (felt extra)

--- ideabase.php
+++ ideabase.php
@@ -2,6 +2,5 @@
     include("pages/ideabase/IdeaBaseHead.html");
     include("template/nav.html");
     include("pages/ideabase/IdeaBaseBody.html");
-    include("template/header.html");
     include("template/footer.html");
 ?>
