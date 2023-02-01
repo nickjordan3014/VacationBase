f301ada55f8eeb6fa55909488e35ca42d65fa606
MykeriaC <mykeriacooks6@knights.ucf.edu>
2023-02-01 13:23:50 -0500

    additions
    added article and made changes to itinerary

---
+++ article.php
@@ -0,0 +1,6 @@
+<?php
+    include("pages/blogsarticles/articleHead.html");
+    include("template/nav.html");
+    include("pages/blogsarticles/articleBody.html");
+    include("template/footer.html");
+?>
