d109200e4cfa38bf493176d65b56d2765ace20f7
KOB20 <59852613+KOB20@users.noreply.github.com>
2023-02-02 23:48:47 -0500

    preplanned itinerary (needs font fix)
    Added a preplanned.php and made a preplannedBody.html using the same code as activityBody.html

---
+++ preplanned.php
@@ -0,0 +1,7 @@
+<?php
+    include("pages/activity/activityHead.html");
+    include("template/nav.html");
+    include("template/header.html");
+    include("pages/activity/preplannedBody.html");
+    include("template/footer.html");
+?>
