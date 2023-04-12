d7ef4918ee7a1a4fa092ba746c11c3705aa16a3e
Nicholas Jordan <65570462+nickjordan3014@users.noreply.github.com>
2023-04-12 14:32:05 -0400

    itinerary + pdf edits

---
+++ js/savetopdf.js
@@ -0,0 +1,14 @@
+function pdfInit() {
+    document.getElementById('saveAsPDF').onclick = function () {
+        console.log("help me please :(")
+        let makepdf = document.getElementById("saveToPDF");
+        let mywindow = window.open("", "PRINT");
+        mywindow.document.write("<head><link rel='stylesheet' href='http://localhost/GitHub/VacationBase/style/main.css'></head>")
+        mywindow.document.write(makepdf.innerHTML);
+        mywindow.document.close();
+        mywindow.focus();
+        mywindow.print();
+    }
+};
+
+window.addEventListener('load', pdfInit, false);
--- pages/itinerary/itineraryBody.php
+++ pages/itinerary/itineraryBody.php
@@ -4,17 +4,7 @@
         
 ?>
 <section class="main-content">
-<!-- 
-TODOS
 
- - Pass added activities into Half Content 1 Section as Draggables
-
- - Users will then be able to drag activities into half content 2 in the 5 Premade day containers
-
- - Save / Submit Button will save Itinerary into user itineraries where they can access on home page
-
- - May want to add a mobile view that says must use computer to access this feature or figure out how js touch events work
--->
 <?php
     // print($_SESSION['user']['id']);
     // print("hello");
@@ -27,33 +17,33 @@ TODOS
         <p class="draggable" draggable="true">2</p>
     </div>
 </section>
+
 <section class="half-content-2">
+
     <h2 class="itinerary-header">Itinerary Builder</h2>
-    <h3>Day 1</h3>
-    <div class="container-itin">
-        <p class="draggable" draggable="true">3</p>
-        <p class="draggable" draggable="true">4</p>
-    </div>
-    <h3>Day 2</h3>
-    <div class="container-itin">
-        <p class="draggable" draggable="true">5</p>
-        <p class="draggable" draggable="true">6</p>
-    </div>
-    <h3>Day 3</h3>
-    <div class="container-itin">
-        <p class="draggable" draggable="true">7</p>
-        <p class="draggable" draggable="true">8</p>
-    </div>
-    <h3>Day 4</h3>
-    <div class="container-itin">
-        <p class="draggable" draggable="true">9</p>
-        <p class="draggable" draggable="true">10</p>
-    </div>
-    <h3>Day 5</h3>
-    <div class="container-itin">
-        <p class="draggable" draggable="true">11</p>
-        <p class="draggable" draggable="true">12</p>
-    </div>
+    <div id="saveToPDF">
+        <h3>Day 1</h3>
+        <div class="container-itin">
+            
+        </div>
+        <h3>Day 2</h3>
+        <div class="container-itin">
+            
+        </div>
+        <h3>Day 3</h3>
+        <div class="container-itin">
+            
+        </div>
+        <h3>Day 4</h3>
+        <div class="container-itin">
+           
+        </div>
+        <h3>Day 5</h3>
+        <div class="container-itin">
+            
+        </div>
+    </div> 
+    <button id="saveAsPDF">Save Itinerary as PDF</button>
 </section>
 
 <?php
