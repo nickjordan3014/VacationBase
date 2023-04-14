fd0f0bd6b861a9e98969754e8f41b40e4de7ffc8
Nicholas Jordan <nickjordan3014@gmail.com>
2023-04-13 21:24:58 -0400

    Some Responsive fixes and extra javascript stuff

---
+++ js/mobile-filter.js
@@ -0,0 +1,9 @@
+function showFilters () {
+    let filterBtn = document.getElementById('filterBtn');
+    filterBtn.classList.remove("filter-content");
+    filterBtn.classList.add("filter-content-on");
+}
+
+function removeFilters() {
+    
+}
--- pages/search/searchHead.php
+++ pages/search/searchHead.php
@@ -1,3 +1,4 @@
     <title>Vacation Base - Search</title>
+    <script src="js/mobile-filter.js"></script>
     <script src="js/autosearch.js"></script>
 </head>
