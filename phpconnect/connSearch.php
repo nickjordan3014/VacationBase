deb8e7ae29e9021cd4e3a91a5cf6b8c48b314aad
KOB20 <59852613+KOB20@users.noreply.github.com>
2023-04-13 20:58:23 -0400

    image naming fix + .jpg in search
    and more resizing + compressing what i missed

--- phpconnect/connSearch.php
+++ phpconnect/connSearch.php
@@ -190,7 +190,7 @@
 
         // temp patch to allow us to access images until we rework how we identify and call them
         $temp_image = explode('/', $card_image);
-        $card_image = ($temp_image[0] . "/image1.jpeg");
+        $card_image = ($temp_image[0] . "/image1.jpg");
 
         // we match the caption from homepage here, but without price since it's a higher priority now that we have more space!
         $card_caption = ( 
